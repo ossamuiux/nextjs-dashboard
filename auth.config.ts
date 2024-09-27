d6bbe8f2d7a176afb24296734457d381a9c211b0
ossamuiux <dalhoya@naver.com>
2024-09-27 15:23:32 +0900

    로그인 인증 추가

---
+++ auth.config.ts
@@ -0,0 +1,23 @@
+import type { NextAuthConfig } from 'next-auth';
+
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      // auth?.user가 undefined, null일 경우 불린값으로 바꾸기위해 !!
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [], // Add providers with an empty array for now
+} satisfies NextAuthConfig;
